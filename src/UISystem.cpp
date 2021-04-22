c97cf5130b2371ed6d6e4958aea76404249942a8
jacker <shogaiam@gmail.com>
2021-04-22 20:08:25 +0300

    imgui testing works

--- src/UISystem.cpp
+++ src/UISystem.cpp
@@ -17,7 +17,7 @@ namespace mt {
 		(void) io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		// io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 
 		// Setup Dear ImGui style
 		ImGui::StyleColorsDark();
@@ -49,26 +49,108 @@ namespace mt {
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		ImGui::Begin(
-				"Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-		ImGui::Checkbox("Another Window", &show_another_window);
+		if (true)
+		{
+			if (ImGui::BeginMainMenuBar())
+			{
 
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-		ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
+				if (ImGui::BeginMenu("File"))
+				{
+					ImGui::Separator();
 
-		if (ImGui::Button("Button"))
-		{                            // Buttons return true when clicked (most widgets return true when edited/activated)
-			counter++;
+					if (ImGui::MenuItem("Quit", "Cmd+Q", false, false))
+					{
+						// Emit quit message
+					}
+
+					ImGui::EndMenu();
+				}
+
+				if (ImGui::BeginMenu("Options"))
+				{
+					// Disabling fullscreen would allow the window to be moved to the front of other windows,
+					// which we can't undo at the moment without finer window depth/z control.
+					if (ImGui::MenuItem("Fullscreen", nullptr, false))
+					{
+
+					}
+					if (ImGui::MenuItem("Padding", nullptr, false))
+					{
+
+					}
+					ImGui::Separator();
+
+					if (ImGui::MenuItem("Close", nullptr, false, false))
+					{
+
+					}
+
+					ImGui::Separator();
+
+					if (ImGui::MenuItem("Quit", nullptr, false, false))
+					{
+
+					}
+
+					ImGui::EndMenu();
+				}
+
+				ImGui::EndMainMenuBar();
+			}
+
+			ImGuiWindowFlags window_flags = 0; //ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+
+			// Create a window called "Hello, world!" and append into it.
+			ImGui::Begin("Hello, world!", nullptr, window_flags);
+
+			ImGui::Text(
+					"This is some useful text.");               // Display some text (you can use a format strings too)
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
+
+			if (ImGui::Button("Button"))
+			{                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				counter++;
+			}
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+						ImGui::GetIO().Framerate);
+
+			if (ImGui::BeginMenuBar())
+			{
+				if (ImGui::BeginMenu("Options"))
+				{
+					// Disabling fullscreen would allow the window to be moved to the front of other windows,
+					// which we can't undo at the moment without finer window depth/z control.
+					if (ImGui::MenuItem("Fullscreen", NULL, false))
+					{
+
+					}
+					if (ImGui::MenuItem("Padding", NULL, false))
+					{
+
+					}
+					ImGui::Separator();
+
+					if (ImGui::MenuItem("Close", NULL, false, false))
+					{
+
+					}
+					ImGui::EndMenu();
+				}
+
+				ImGui::EndMenuBar();
+			}
+
+			ImGui::End();
 		}
-		ImGui::SameLine();
-		ImGui::Text("counter = %d", counter);
-
-		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
-					ImGui::GetIO().Framerate);
-		ImGui::End();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
