349ce9158271189051a57c9cb64dcff21f67e14f
jacker <shogaiam@gmail.com>
2021-04-26 00:08:42 +0300

    switch to C++ 17

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,59 +1,60 @@
 #include "filesystem.h"
 #include "io.h"
 
-namespace mt {
-	namespace IO {
+namespace mt::IO {
 
-		MTPath::MTPath(std::string &path) : path(path)
+	MTPath::MTPath(std::string &path) : path(path)
+	{
+		// We only use '/' internally so sanitize the paths
+		for (char &character : this->path)
 		{
-			// We only use '/' internally so sanitize the paths
-			for (char &character : this->path) {
-				if (character == '\\') {
-					character = '/';
-				}
+			if (character == '\\')
+			{
+				character = '/';
 			}
 		}
+	}
 
-		// Nothing to do here really
-		MTPath::~MTPath() = default;
+	// Nothing to do here really
+	MTPath::~MTPath() = default;
 
-		bool MTPath::findFile(const std::string &name)
-		{
-			return FileExists(this->path + path);
-		}
+	bool MTPath::findFile(const std::string &name)
+	{
+		return FileExists(this->path + path);
+	}
 
-		MTFile *MTPath::loadFile(const std::string &name)
-		{
-			byte* data = FileRead(this->path + path);
-			if (!data) {
-				return nullptr;
-			}
-
-			auto *file = new MTFile;
-			file->data = data;
-			file->name = name;
-
-			return file;
-		}
-
-		MTPackage::MTPackage(std::string &path)
-		{
-
-		}
-
-		MTPackage::~MTPackage()
-		{
-
-		}
-
-		bool MTPackage::findFile(const std::string &name)
-		{
-			return false;
-		}
-
-		MTFile *MTPackage::loadFile(const std::string &name)
+	MTFile *MTPath::loadFile(const std::string &name)
+	{
+		byte *data = FileRead(this->path + path);
+		if (!data)
 		{
 			return nullptr;
 		}
+
+		auto *file = new MTFile;
+		file->data = data;
+		file->name = name;
+
+		return file;
+	}
+
+	MTPackage::MTPackage(std::string &path)
+	{
+
+	}
+
+	MTPackage::~MTPackage()
+	{
+
+	}
+
+	bool MTPackage::findFile(const std::string &name)
+	{
+		return false;
+	}
+
+	MTFile *MTPackage::loadFile(const std::string &name)
+	{
+		return nullptr;
 	}
 }
--- src/filesystem.h
+++ src/filesystem.h
@@ -5,62 +5,61 @@
 #include <string>
 #include <vector>
 
-namespace mt {
-	namespace IO {
+namespace mt::IO {
 
-		struct MTFile {
-			byte *data = nullptr;
-			std::string name;
-			std::string ext;
-			size_t len = 0;
+	struct MTFile {
+		byte *data = nullptr;
+		std::string name;
+		std::string ext;
+		size_t len = 0;
 
-			MTFile() = default;
+		MTFile() = default;
 
-			~MTFile() = default;
-		};
+		~MTFile() = default;
+	};
 
-		class FileSource {
-		public:
-			virtual ~FileSource() = default;
+	class FileSource {
+	public:
+		virtual ~FileSource() = default;
 
-			virtual bool findFile(const std::string &name) = 0;
+		virtual bool findFile(const std::string &name) = 0;
 
-			virtual MTFile *loadFile(const std::string &name) = 0;
-		};
+		virtual MTFile *loadFile(const std::string &name) = 0;
+	};
 
-		class MTPath : FileSource {
-		public:
-			explicit MTPath(std::string &path);
+	class MTPath : FileSource {
+	public:
+		explicit MTPath(std::string &path);
 
-			~MTPath() override;
+		~MTPath() override;
 
-			bool findFile(const std::string &name) override;
+		bool findFile(const std::string &name) override;
 
-			MTFile *loadFile(const std::string &name) override;
-		private:
-			std::string path;
-		};
+		MTFile *loadFile(const std::string &name) override;
 
-		class MTPackage : FileSource {
-		public:
-			explicit MTPackage(std::string &path);
+	private:
+		std::string path;
+	};
 
-			~MTPackage() override;
+	class MTPackage : FileSource {
+	public:
+		explicit MTPackage(std::string &path);
 
-			bool findFile(const std::string &name) override;
+		~MTPackage() override;
 
-			MTFile *loadFile(const std::string &name) override;
-		};
+		bool findFile(const std::string &name) override;
 
-		class FileSystem {
-		public:
-			FileSystem();
+		MTFile *loadFile(const std::string &name) override;
+	};
 
-			~FileSystem();
+	class FileSystem {
+	public:
+		FileSystem();
 
-		private:
-			std::vector<FileSource> sources;
-		};
-	}
+		~FileSystem();
+
+	private:
+		std::vector<FileSource> sources;
+	};
 }
 
