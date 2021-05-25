30c10f215cae020c649920e695516cc2d37f21a3
jacker <shogaiam@gmail.com>
2021-05-26 01:43:23 +0300

    adde the missing loader call

--- src/app/ModelLoader.cpp
+++ src/app/ModelLoader.cpp
@@ -1,20 +1,30 @@
 #include "ModelLoader.h"
 #include "ModelMD5.h"
 
-mt::Model *mt::model::ModelLoader::load(const mt::Ref<mt::IO::MTFile> &)
+mt::Model *mt::model::ModelLoader::load(const mt::Ref<mt::IO::MTFile> &file)
 {
+	// Make sure the loader map is initialized
 	init();
 
-	return nullptr;
+	auto it = std::find_if(std::begin(loaders), std::end(loaders),
+						   [&](auto &&p) { return p->first == file->ext; });
+
+	if (it == std::end(loaders))
+	{
+		throw std::invalid_argument("Unknown extension.");
+	}
+
+	return it->second(file);
 }
 
 void mt::model::ModelLoader::init()
 {
-	if (!ModelLoader::loaders.empty()) {
+	if (!loaders.empty())
+	{
 		return;
 	}
 
-	ModelLoader::loaders.emplace(std::string("md5"), mt::model::loadMD5);
+	loaders.emplace(std::string("md5"), mt::model::loadMD5);
 
 	// FIXME: register the the missing model formats here
 }
