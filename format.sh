441eb890efd3ed9bb7bef6395e7537cdfc666124
Jacker <shogaiam@gmail.com>
2024-10-08 15:31:03 +0300

    Added clang-format and clang-tidy configurations

---
+++ format.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is used to format the code in the repository.
+find src/ -iname '*.h' -o -iname '*.cpp' | xargs clang-format -i
+# find src/ -iname '*.h' -o -iname '*.cpp' | xargs clang-tidy -checks='-*,modernize-concat-nested-namespaces' -fix
